2f07e3c8e94ea026aaa6f22ca6a8b1120de6e5a7
cho-yewon <q5556@naver.com>
2023-05-19 17:59:48 +0900

    Add files via upload

---
+++ c_9/20230427-26.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double a, b;
+
+    a = (0.03 * 3) + 0.1;
+    b = 1;
+
+    printf("a==b의 결과 : %d\n", a == b);
+    ptinrf("fabs(a-b)<0.00001의 결과 : %d\n", fabs(a - b) < 0.00001);
+    
+    return 0;
+}
